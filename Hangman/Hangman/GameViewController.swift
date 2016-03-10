da9e87cbbfd7a05b061d5189a68e4e8cf3412950
Siddhant K Tandon <sidt101@berkeley.edu>
2016-03-10 01:23:26 -0800

    Part 1 Completed

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -10,19 +10,87 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var wordToBeGuessed: UILabel!
+    
+    @IBOutlet weak var correctButton: UIButton!
+    
+    @IBOutlet weak var incorrectButton: UIButton!
+    
+    @IBOutlet weak var hangmanStateImage: UIImageView!
+    
+    @IBOutlet weak var incorrectGuesses: UILabel!
+    
+    @IBOutlet weak var letterGuessed: UITextField!
+    
+    var globalPhrase:String = ""
+    var unfilledBlanks: String = ""
+    var incorrectGuessesString: String = ""
+    var numberOfIncorrectGuesses: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        var phrase = hangmanPhrases.getRandomPhrase()
-        print(phrase)
+        var phraseToGuess = hangmanPhrases.getRandomPhrase()
+        globalPhrase = phraseToGuess
+        print(phraseToGuess)
+        
+        let x = phraseToGuess.characters.count
+        for index in 0...x-1 {
+            let characterOfGuessWord = phraseToGuess[phraseToGuess.startIndex.advancedBy(index)]
+            if characterOfGuessWord != " " {
+                unfilledBlanks += "-"
+            } else {
+                unfilledBlanks += " "
+            }
+        }
+        wordToBeGuessed.text = unfilledBlanks
+        hangmanStateImage.image = UIImage(named: "hangman1.gif")
+        playGame()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func playGame() {
+        
+                correctButton.addTarget(self, action: "guessedCorrectly:", forControlEvents: .TouchUpInside)
+                incorrectButton.addTarget(self, action: "guessedIncorrectly:", forControlEvents: .TouchUpInside)
+    }
+    
+    func guessedCorrectly(sender:UIButton!) {
+        let x = unfilledBlanks.characters.count
+        for index in 0...x-1 {
+            let characterOfGuessWord = unfilledBlanks[unfilledBlanks.startIndex.advancedBy(index)]
+            if characterOfGuessWord == "-" {
+                let aRange = Range<String.Index>(start: unfilledBlanks.startIndex.advancedBy(index), end: unfilledBlanks.startIndex.advancedBy(index+1))
+                print(globalPhrase[globalPhrase.startIndex.advancedBy(index)])
+                
+                unfilledBlanks = unfilledBlanks.stringByReplacingCharactersInRange(aRange, withString: String(globalPhrase[globalPhrase.startIndex.advancedBy(index)]))
+                
+                print(unfilledBlanks)
+                wordToBeGuessed.text = unfilledBlanks
+                break
+            }
+        }
+    }
+    
+    func guessedIncorrectly(sender:UIButton!) {
+        
+        if letterGuessed.text != "" && letterGuessed.text?.characters.count == 1 {
+            numberOfIncorrectGuesses += 1
+            hangmanStateImage.image = UIImage(named: "hangman" + String(numberOfIncorrectGuesses + 1) + ".gif")
+            incorrectGuesses.text = incorrectGuesses.text! + " " + letterGuessed.text!
+        }
+        
+        
+    }
+    
     
 
     /*
--- Hangman/Hangman/StartScreenViewController.swift
+++ Hangman/Hangman/StartScreenViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class StartScreenViewController: UIViewController {
 
+    @IBOutlet weak var startGameButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
