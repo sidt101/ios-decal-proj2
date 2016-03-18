80567fe77afe814cc0800e9305b7f812ab53173d
Siddhant K Tandon <sidt101@berkeley.edu>
2016-03-17 19:21:50 -0700

    Completed hangman

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -12,9 +12,7 @@ class GameViewController: UIViewController {
 
     @IBOutlet weak var wordToBeGuessed: UILabel!
     
-    @IBOutlet weak var correctButton: UIButton!
-    
-    @IBOutlet weak var incorrectButton: UIButton!
+    @IBOutlet weak var guessButton: UIButton!
     
     @IBOutlet weak var hangmanStateImage: UIImageView!
     
@@ -26,6 +24,7 @@ class GameViewController: UIViewController {
     var unfilledBlanks: String = ""
     var incorrectGuessesString: String = ""
     var numberOfIncorrectGuesses: Int = 0
+    var allGuesses = Set<Character>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,35 +58,94 @@ class GameViewController: UIViewController {
     
     func playGame() {
         
-                correctButton.addTarget(self, action: "guessedCorrectly:", forControlEvents: .TouchUpInside)
-                incorrectButton.addTarget(self, action: "guessedIncorrectly:", forControlEvents: .TouchUpInside)
+        guessButton.addTarget(self, action: "makeAGuess:", forControlEvents: .TouchUpInside)
     }
     
-    func guessedCorrectly(sender:UIButton!) {
-        let x = unfilledBlanks.characters.count
-        for index in 0...x-1 {
-            let characterOfGuessWord = unfilledBlanks[unfilledBlanks.startIndex.advancedBy(index)]
-            if characterOfGuessWord == "-" {
-                let aRange = Range<String.Index>(start: unfilledBlanks.startIndex.advancedBy(index), end: unfilledBlanks.startIndex.advancedBy(index+1))
-                print(globalPhrase[globalPhrase.startIndex.advancedBy(index)])
-                
-                unfilledBlanks = unfilledBlanks.stringByReplacingCharactersInRange(aRange, withString: String(globalPhrase[globalPhrase.startIndex.advancedBy(index)]))
-                
-                print(unfilledBlanks)
-                wordToBeGuessed.text = unfilledBlanks
-                break
+    func validateLetterGuessed(character: String) -> Bool {
+        
+        if character == "" || (character.characters.count) != 1 {
+            return false;
+        }
+        
+        let guessedLetter = character.uppercaseString
+        let unicodeValueOfCharacter = Int(guessedLetter.unicodeScalars[guessedLetter.unicodeScalars.startIndex].value)
+        
+        if (unicodeValueOfCharacter > 90 || unicodeValueOfCharacter < 65) {
+            let alert = UIAlertController(title: "Incorrect Input", message: "Only Alphabets are allowed!", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "I'll Be Careful Next Time", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return false;
+        }
+        
+        if allGuesses.contains(Character(guessedLetter)) {
+            let alert = UIAlertController(title: "Incorrect Input", message: "You have already guessed this letter! Try another one.", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "I'll Be Careful Next Time", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return false;
+        }
+        
+        return true;
+    }
+    
+    func makeAGuess(sender:UIButton!) {
+        
+        var guessedCorrectly = false;
+        if let text = letterGuessed.text where !text.isEmpty && (validateLetterGuessed(text)) {
+            let guessedCharacter = Character(text.uppercaseString)
+            allGuesses.insert(guessedCharacter)
+            let numberOfCharacters = unfilledBlanks.characters.count
+            for index in 0...numberOfCharacters-1 {
+                let characterOfHiddenPhrase = globalPhrase[globalPhrase.startIndex.advancedBy(index)]
+                if characterOfHiddenPhrase == guessedCharacter {
+                    guessedCorrectly = true
+                    let aRange = Range<String.Index>(start: unfilledBlanks.startIndex.advancedBy(index), end: unfilledBlanks.startIndex.advancedBy(index+1))
+                    unfilledBlanks = unfilledBlanks.stringByReplacingCharactersInRange(aRange, withString: String(characterOfHiddenPhrase))
+                    
+                }
+            }
+            
+            wordToBeGuessed.text = unfilledBlanks
+            if (!guessedCorrectly) {
+                guessedIncorrectly(guessedCharacter)
+            } else {
+                if checkForWin() {
+                    let alert = UIAlertController(title: "Congratulations, you won!", message: "You have won the impossible game of Hangman.", preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title: "I had soooo much fun!", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                    guessButton.enabled = false
+                    
+                }
+            }
+            guessedCorrectly = false
+            letterGuessed.text = ""
+        }
+        
+        else {
+            letterGuessed.text = ""
+        }
+    }
+    
+    func checkForWin() -> Bool {
+        let numberOfCharacters = unfilledBlanks.characters.count
+        for index in 0...numberOfCharacters-1 {
+            let characterOfHiddenPhrase = unfilledBlanks[unfilledBlanks.startIndex.advancedBy(index)]
+            if characterOfHiddenPhrase == "-" {
+                return false
             }
         }
+        return true
     }
     
-    func guessedIncorrectly(sender:UIButton!) {
-        
-        if letterGuessed.text != "" && letterGuessed.text?.characters.count == 1 {
-            numberOfIncorrectGuesses += 1
-            hangmanStateImage.image = UIImage(named: "hangman" + String(numberOfIncorrectGuesses + 1) + ".gif")
-            incorrectGuesses.text = incorrectGuesses.text! + " " + letterGuessed.text!
+    func guessedIncorrectly(guessedCharacter:Character) {
+        numberOfIncorrectGuesses += 1
+        hangmanStateImage.image = UIImage(named: "hangman" + String(numberOfIncorrectGuesses + 1) + ".gif")
+        incorrectGuesses.text = incorrectGuesses.text! + " " + String(guessedCharacter)
+        if numberOfIncorrectGuesses == 7 {
+            let alert = UIAlertController(title: "You Lost :(", message: "You have run out of moves", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Gotta practice my words.", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            guessButton.enabled = false
         }
-        
         
     }
     
